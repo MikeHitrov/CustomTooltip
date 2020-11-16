b3dbecc62f2bbd2132daf232a882cad2540f53eb
mikeHitrov <mike.hitrov@gmail.com>
2020-11-16 15:01:05 +0200

    Updated the show method

--- scripts/script.js
+++ scripts/script.js
@@ -3,7 +3,10 @@ function addTooltip(elementId, text, delayBeforeShow, delayAfterShow) {
   const element = document.getElementById(elementId);
 
   //Creating the base element
-  const base = document.createElement("tooltip");
+  const base = document.getElementById("tooltip");
+
+  //Defining the offset distance
+  const offsetDistance = 35;
 
   //Adding stylesheet
   base.classList.add("tooltip");
@@ -14,12 +17,15 @@ function addTooltip(elementId, text, delayBeforeShow, delayAfterShow) {
       base.innerHTML = text;
 
       // Checking for any "tooltip" element
-      if (document.getElementsByTagName("tooltip")[0]) {
-        document.getElementsByTagName("tooltip")[0].remove(); // Removing old tooltip
+      if (base.style.display == "inline-block") {
+        base.style.display = "none"; // Hiding old tooltip
       }
 
       //Setting the timeout before the tooltip shows
       setTimeout(() => {
+        base.style.top = element.offsetTop + offsetDistance + 'px';
+        base.style.left = element.offsetLeft + offsetDistance + 'px';
+        base.style.display = 'inline-block';
         document.body.appendChild(base);
       }, delayBeforeShow);
     }
@@ -28,7 +34,7 @@ function addTooltip(elementId, text, delayBeforeShow, delayAfterShow) {
   element.addEventListener("mouseleave", (event) => {
     //Setting the timeout before the tooltip is being removes
     setTimeout(() => {
-      document.getElementsByTagName("tooltip")[0].remove(); //Removing the tooltip
+      base.style.display = "none"; //Hiding the tooltip
     }, delayAfterShow);
   });
 }
