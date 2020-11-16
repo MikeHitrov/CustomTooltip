e956d0c48e3edcfac67b99a696c253ef2ed78c31
mikeHitrov <mike.hitrov@gmail.com>
2020-11-16 15:06:57 +0200

    Fixed the tests

--- cypress/integration/test.js
+++ cypress/integration/test.js
@@ -3,8 +3,8 @@ describe("Open the site", () => {
     cy.visit("../../pages/index.html");
     cy.get("#element").trigger("mouseover");
     cy.get("#element").trigger("mouseleave");
-    cy.get("tooltip").should("exist").should('have.text', 'Test');
+    cy.get("#tooltip").should("be.visible").should('have.text', 'Test');
     cy.wait(1000);
-    cy.get("tooltip").should("not.exist");
+    cy.get("#tooltip").should("not.be.visible");
   });
 });
